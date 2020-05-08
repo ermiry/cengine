775cce4a520f2396a443cf8fddf2d0c11c74d37a
ermiry <erick_salas@ermiry.com>
2020-05-07 19:10:29 -0500

    Added new files methods

--- src/cengine/files.c
+++ src/cengine/files.c
@@ -5,10 +5,65 @@
 #define _XOPEN_SOURCE 700
 #include <sys/types.h>
 #include <sys/stat.h>
+#include <dirent.h>
+
+#include "cengine/types/string.h"
+#include "cengine/collections/dlist.h"
 
 #include "cengine/utils/utils.h"
 #include "cengine/utils/log.h"
 #include "cengine/utils/json.h"
+
+static String *file_get_line (FILE *file) {
+
+    String *str = NULL;
+
+    if (file) {
+        if (!feof (file)) {
+            char line[1024];
+            if (fgets (line, 1024, file)) {
+                size_t curr = strlen(line);
+                if(line[curr - 1] == '\n') line[curr - 1] = '\0';
+
+                str = str_new (line);
+            }
+        }
+    }
+
+    return str;
+
+}
+
+// reads eachone of the file's lines into a newly created string and returns them inside a dlist
+DoubleList *file_get_lines (const char *filename) {
+
+    DoubleList *lines = NULL;
+
+    if (filename) {
+        FILE *file = fopen (filename, "r");
+        if (file) {
+            lines = dlist_init (str_delete, str_comparator);
+            
+            String *line = NULL;
+            while ((line = file_get_line (file))) {
+                dlist_insert_after (lines, dlist_end (lines), line);
+            }
+
+            fclose (file);
+        }
+
+        else {
+            char *status = c_string_create ("Failed to open file: %s", filename);
+            if (status) {
+                cengine_log_error (status);
+                free (status);
+            }
+        }
+    }
+
+    return lines;
+
+}
 
 // returns an allocated string with the file extensio
 // NULL if no file extension
@@ -39,7 +94,46 @@ char *files_get_file_extension (const char *filename) {
 
 }
 
-FILE *file_open (const char *filename, const char *modes, struct stat *filestatus) {
+// returns a list of strings containg the names of all the files in the directory
+DoubleList *files_get_from_dir (const char *dir) {
+
+    DoubleList *images = NULL;
+
+    if (dir) {
+        DIR *dp;
+        struct dirent *ep;
+
+		images = dlist_init (str_delete, str_comparator);
+
+        dp = opendir (dir);
+        if (dp) {
+            String *file = NULL;
+            while ((ep = readdir (dp)) != NULL) {
+                if (strcmp (ep->d_name, ".") && strcmp (ep->d_name, "..")) {
+                    file = str_create ("%s/%s", dir, ep->d_name);
+
+                    dlist_insert_after (images, dlist_end (images), file);
+                }
+            }
+
+            (void) closedir (dp);
+        }
+
+        else {
+			char *status = c_string_create ("Failed to open dir %s", dir);
+			if (status) {
+				cengine_log_error (status);
+				free (status);
+			}
+        }
+    }
+
+    return images;
+
+}
+
+// opens a file and returns it as a FILE
+FILE *file_open_as_file (const char *filename, const char *modes, struct stat *filestatus) {
 
     FILE *fp = NULL;
 
@@ -48,8 +142,12 @@ FILE *file_open (const char *filename, const char *modes, struct stat *filestatu
         if (!stat (filename, filestatus)) 
             fp = fopen (filename, modes);
 
-        else cengine_log_msg (stderr, LOG_ERROR, LOG_NO_TYPE, 
-            c_string_create ("File %s not found!", filename));
+        else {
+            #ifdef CERVER_DEBUG
+            cerver_log_msg (stderr, LOG_ERROR, LOG_FILE, 
+                c_string_create ("File %s not found!", filename));
+            #endif
+        } 
     }
 
     return fp;
@@ -57,27 +155,37 @@ FILE *file_open (const char *filename, const char *modes, struct stat *filestatu
 }
 
 // opens and reads a file into a buffer
+// sets file size to the amount of bytes read
 char *file_read (const char *filename, int *file_size) {
 
     char *file_contents = NULL;
 
     if (filename) {
         struct stat filestatus;
-        FILE *fp = file_open (filename, "rt", &filestatus);
+        FILE *fp = file_open_as_file (filename, "rt", &filestatus);
         if (fp) {
             *file_size = filestatus.st_size;
             file_contents = (char *) malloc (filestatus.st_size);
 
             // read the entire file into the buffer
             if (fread (file_contents, filestatus.st_size, 1, fp) != 1) {
-                cengine_log_msg (stderr, LOG_ERROR, LOG_NO_TYPE, "Failed to read file contents!");
+                #ifdef CERVER_DEBUG
+                cerver_log_msg (stderr, LOG_ERROR, LOG_FILE, 
+                    c_string_create ("Failed to read file (%s) contents!"));
+                #endif
+
                 free (file_contents);
             }
 
             fclose (fp);
         }
 
-        else cengine_log_msg (stderr, LOG_ERROR, LOG_NO_TYPE, c_string_create ("Unable to open file %s", filename));
+        else {
+            #ifdef CERVER_DEBUG
+            cerver_log_msg (stderr, LOG_ERROR, LOG_FILE, 
+                c_string_create ("Unable to open file %s.", filename));
+            #endif
+        }
     }
 
     return file_contents;
