a532d71ad8ee9f37e284e471271bd2fdb0481123
ermiry <erick_salas@ermiry.com>
2020-06-27 16:18:43 -0500

    Refactor main thread methods with the ones from latest cerver

--- include/cengine/threads/thread.h
+++ include/cengine/threads/thread.h
@@ -1,26 +1,13 @@
-#ifndef _CENGINE_THREAD_H_
-#define _CENGINE_THREAD_H_
+#ifndef _CENGINE_THREADS_H_
+#define _CENGINE_THREADS_H_
 
-#include <SDL2/SDL.h>
+#include <pthread.h>
 
-#include "cengine/os.h"
-
-#if defined OS_LINUX
-    #include <pthread.h>
-#endif
-
-#include "cengine/types/types.h"
-#include "cengine/types/string.h"
-
-#include "cengine/collections/dlist.h"
-
-#define THREAD_OK   0
+#include "cerver/types/types.h"
 
 // creates a custom detachable thread (will go away on its own upon completion)
-// handle manually in linux, with no name
-// in any other platform, created with sdl api and you can pass a custom name
 // returns 0 on success, 1 on error
-extern u8 thread_create_detachable (void *(*work) (void *), void *args);
+extern u8 thread_create_detachable (pthread_t *thread, void *(*work) (void *), void *args);
 
 // sets thread name from inisde it
 extern int thread_set_name (const char *name);
--- src/cengine/threads/thread.c
+++ src/cengine/threads/thread.c
@@ -1,44 +1,35 @@
-#include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
+#include <stdio.h>
 
-#include <SDL2/SDL.h>
+#include <errno.h>
 
-#include "cengine/os.h"
-
-#if defined OS_LINUX
-    #include <sys/prctl.h>
-#endif
+#include <pthread.h>
+#include <sys/prctl.h>
 
 #include "cengine/types/types.h"
-#include "cengine/threads/thread.h"
 #include "cengine/utils/log.h"
-#include "cengine/utils/utils.h"
 
 // creates a custom detachable thread (will go away on its own upon completion)
-// handle manually in linux, with no name
-// in any other platform, created with sdl api and you can pass a custom name
-u8 thread_create_detachable (void *(*work) (void *), void *args) {
+// returns 0 on success, 1 on error
+u8 thread_create_detachable (pthread_t *thread, void *(*work) (void *), void *args) {
 
     u8 retval = 1;
 
-    #ifdef OS_LINUX
+    if (thread) {
         pthread_attr_t attr;
-        pthread_t thread;
+        if (!pthread_attr_init (&attr)) {
+            if (!pthread_attr_setdetachstate (&attr, PTHREAD_CREATE_DETACHED)) {
+                if (!pthread_create (thread, &attr, work, args) != THREAD_OK) {
+                    retval = 0;     // success
+                }
 
-        int rc = pthread_attr_init (&attr);
-        rc = pthread_attr_setdetachstate (&attr, PTHREAD_CREATE_DETACHED);
-
-        if (pthread_create (&thread, &attr, work, args) != THREAD_OK)
-            cengine_log_msg (stderr, LOG_ERROR, LOG_NO_TYPE, "Failed to create detachable thread!");
-        else retval = 0;
-    #else
-        SDL_Thread *thread = SDL_CreateThread ((int (*) (void *)) work, NULL, args);
-        if (thread) {
-            SDL_DetachThread (thread);  // will go away on its own upon completion
-            retval = 0;
+                else {
+                    cerver_log_msg (stderr, LOG_ERROR, LOG_NO_TYPE, "Failed to create detachable thread!");
+                    perror ("Error");
+                }
+            }
         }
-    #endif
+    }
 
     return retval;
 
@@ -47,19 +38,7 @@ u8 thread_create_detachable (void *(*work) (void *), void *args) {
 // sets thread name from inisde it
 int thread_set_name (const char *name) {
 
-    int retval = 1;
-
-    if (name) {
-        #if defined     OS_LINUX
-            // use prctl instead to prevent using _GNU_SOURCE flag and implicit declaration
-            retval = prctl (PR_SET_NAME, name);
-        #elif defined   OS_MACOS
-            retval = pthread_setname_np (name);
-        #elif defined   CENGINE_DEBUG
-            cengine_log_msg (stdout, LOG_WARNING, LOG_NO_TYPE, "pthread_setname_np is not supported on this system.");
-        #endif
-    }
-
-    return retval;
+    // use prctl instead to prevent using _GNU_SOURCE flag and implicit declaration
+    return prctl (PR_SET_NAME, name);
 
 }
