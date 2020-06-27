6bf79d272fb4fbf6055c57c759916576b727ac84
ermiry <erick_salas@ermiry.com>
2020-06-27 16:17:13 -0500

    Removed old thread hub

--- include/cengine/threads/thread.h
+++ include/cengine/threads/thread.h
@@ -25,38 +25,4 @@ extern u8 thread_create_detachable (void *(*work) (void *), void *args);
 // sets thread name from inisde it
 extern int thread_set_name (const char *name);
 
-typedef struct HubWorker {
-
-    pthread_t pthread;
-    SDL_Thread *thread;
-
-    String *name;
-    void *(*job) (void *);
-    void *args;
-
-} HubWorker;
-
-typedef struct ThreadHub {
-
-    String *name;
-    DoubleList *threads;
-
-} ThreadHub;
-
-// inits a new thread hub
-extern ThreadHub *thread_hub_int (const char *name);
-
-// inits the global thread hub
-extern int thread_hub_init_global (void);
-extern void thread_hub_end_global (void);
-
-// ends a thread hub
-extern void thread_hub_end (ThreadHub *hub);
-
-// adds a worker to the hub
-extern int thread_hub_add (ThreadHub *hub, void *(*work) (void *), void *args, const char *worker_name);
-
-// removes a worker from the hub
-extern int thread_hub_remove (ThreadHub *hub, const char *worker_name);
-
 #endif
