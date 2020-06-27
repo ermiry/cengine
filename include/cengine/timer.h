da8663c746aaa4f5b857ac2a4f5fb61cfa85806a
ermiry <erick_salas@ermiry.com>
2020-06-27 16:34:44 -0500

    Merged client timer source with existing cengine ones

--- include/cengine/timer.h
+++ include/cengine/timer.h
@@ -1,12 +1,30 @@
 #ifndef _CENGINE_TIMER_H_
 #define _CENGINE_TIMER_H_
 
+#ifndef __USE_POSIX199309
+    #define __USE_POSIX199309
+#endif
+
 #include <stdbool.h>
+#include <time.h>
 
 #include "cengine/types/types.h"
 #include "cengine/types/string.h"
 
+#pragma region time
+
+typedef struct timespec TimeSpec;
+
+extern void timespec_delete (void *timespec_ptr);
+
+extern TimeSpec *timer_get_timespec (void);
+
+extern double timer_elapsed_time (TimeSpec *start, TimeSpec *end);
+
+extern void timer_sleep_for_seconds (double seconds);
+
 extern struct tm *timer_get_gmt_time (void);
+
 extern struct tm *timer_get_local_time (void);
 
 // returns a string representing the 24h time 
@@ -21,6 +39,10 @@ extern String *timer_date_and_time_to_string (struct tm *timeinfo);
 // returns a string representing the time with custom format
 extern String *timer_time_to_string_custom (struct tm *timeinfo, const char *format);
 
+#pragma endregion
+
+#pragma region timer
+
 typedef struct Timer {
 
     u32 startTicks;     // the clock time when the timer started
@@ -34,13 +56,19 @@ typedef struct Timer {
 } Timer;
 
 extern Timer *timer_new (void);
+
 extern void timer_destroy (Timer *timer);
 
 extern void timer_start (Timer *timer);
+
 extern void timer_stop (Timer *timer);
+
 extern void timer_pause (Timer *timer);
+
 extern void timer_unpause (Timer *timer);
 
 extern u32 timer_get_ticks (Timer *timer);
+
+#pragma endregion
 
 #endif
--- src/cengine/timer.c
+++ src/cengine/timer.c
@@ -1,5 +1,7 @@
 #include <stdbool.h>
+
 #include <time.h>
+#include <errno.h>
 
 #include <SDL2/SDL.h>
 
@@ -8,9 +10,56 @@
 
 #include "cengine/timer.h"
 
+#pragma region time
+
+static TimeSpec *timespec_new (void) {
+
+    TimeSpec *t = (TimeSpec *) malloc (sizeof (TimeSpec));
+    if (t) {
+        t->tv_nsec = 0;
+        t->tv_sec = 0;
+    }
+
+    return t;
+
+}
+
+void timespec_delete (void *timespec_ptr) { if (timespec_ptr) free (timespec_ptr); }
+
+TimeSpec *timer_get_timespec (void) {
+
+    TimeSpec *t = timespec_new ();
+    if (t) {
+        clock_gettime (4, t);
+    }
+
+    return t;
+
+}
+
+double timer_elapsed_time (TimeSpec *start, TimeSpec *end) {
+
+    return (start && end) ? (double) end->tv_sec + (double) end->tv_nsec / 1000000000
+        - (double) start->tv_sec - (double) start->tv_nsec / 1000000000 : 0;
+
+}
+
+void timer_sleep_for_seconds (double seconds) {
+
+    TimeSpec timespc = { 0 };
+	timespc.tv_sec = (time_t) seconds;
+	timespc.tv_nsec = (long) ((seconds - timespc.tv_sec) * 1e+9);
+
+	int result;
+	do {
+		result = nanosleep (&timespc, &timespc);
+	} while (result == -1 && errno == EINTR);
+
+}
+
 struct tm *timer_get_gmt_time (void) {
 
-    time_t rawtime;
+    time_t rawtime = 0;
     time (&rawtime);
     return gmtime (&rawtime);
 
@@ -18,7 +67,7 @@ struct tm *timer_get_gmt_time (void) {
 
 struct tm *timer_get_local_time (void) {
 
-    time_t rawtime;
+    time_t rawtime = 0;
     time (&rawtime);
     return localtime (&rawtime);
 
@@ -28,8 +77,8 @@ struct tm *timer_get_local_time (void) {
 String *timer_time_to_string (struct tm *timeinfo) {
 
     if (timeinfo) {
-        char buffer[80];
-        strftime (buffer, sizeof (buffer), "%T", timeinfo);
+        char buffer[128] = { 0 };
+        strftime (buffer, 128, "%T", timeinfo);
         return str_new (buffer);
     }
 
@@ -41,8 +90,8 @@ String *timer_time_to_string (struct tm *timeinfo) {
 String *timer_date_to_string (struct tm *timeinfo) {
 
     if (timeinfo) {
-        char buffer[80];
-        strftime (buffer, sizeof (buffer), "%d/%m/%y", timeinfo);
+        char buffer[128] = { 0 };
+        strftime (buffer, 128, "%d/%m/%y", timeinfo);
         return str_new (buffer);
     }
 
@@ -54,8 +103,8 @@ String *timer_date_to_string (struct tm *timeinfo) {
 String *timer_date_and_time_to_string (struct tm *timeinfo) {
 
     if (timeinfo) {
-        char buffer[80];
-        strftime (buffer, sizeof (buffer), "%d/%m/%y - %T", timeinfo);
+        char buffer[128] = { 0 };
+        strftime (buffer, 128, "%d/%m/%y - %T", timeinfo);
         return str_new (buffer);
     }
 
@@ -67,8 +116,8 @@ String *timer_date_and_time_to_string (struct tm *timeinfo) {
 String *timer_time_to_string_custom (struct tm *timeinfo, const char *format) {
 
     if (timeinfo) {
-        char buffer[80];
-        strftime (buffer, sizeof (buffer), format, timeinfo);
+        char buffer[128] = { 0 };
+        strftime (buffer, 128, format, timeinfo);
         return str_new (buffer);
     }
     
@@ -76,15 +125,21 @@ String *timer_time_to_string_custom (struct tm *timeinfo, const char *format) {
 
 }
 
+#pragma endregion
+
+#pragma region timer
+
 static void timer_reset (Timer *timer) {
 
-    timer->startTicks = 0;
-    timer->pausedTicks = 0;
+    if (timer) {
+        timer->startTicks = 0;
+        timer->pausedTicks = 0;
 
-    timer->ticks = 0;
+        timer->ticks = 0;
 
-    timer->isPaused = false;
-    timer->started = false;
+        timer->isPaused = false;
+        timer->started = false;
+    }
 
 }
 
@@ -152,3 +207,5 @@ u32 timer_get_ticks (Timer *timer) {
     return time_ret;
     
 }
+
+#pragma endregion
