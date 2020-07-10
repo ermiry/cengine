f8f53aaed016c4bd328e3f708186625266e449be
ermiry <erick_salas@ermiry.com>
2020-07-10 12:58:47 -0500

    Updated to version 0.9.5rc-3

--- include/cengine/version.h
+++ include/cengine/version.h
@@ -1,10 +1,10 @@
 #ifndef _CENGINE_VERSION_H_
 #define _CENGINE_VERSION_H_
 
-#define CENGINE_VERSION                 "0.9.5rc-2"
-#define CENGINE_VERSION_NAME            "Pre-Release 0.9.5rc-2"
-#define CENGINE_VERSION_DATE			"30/06/2020"
-#define CENGINE_VERSION_TIME			"03:02 CST"
+#define CENGINE_VERSION                 "0.9.5rc-3"
+#define CENGINE_VERSION_NAME            "Pre-Release 0.9.5rc-3"
+#define CENGINE_VERSION_DATE			"10/07/2020"
+#define CENGINE_VERSION_TIME			"12:58 CST"
 #define CENGINE_VERSION_AUTHOR			"Erick Salas"
 
 // print full cengine version information 
