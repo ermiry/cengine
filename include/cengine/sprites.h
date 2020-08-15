f6bf598c73ef526c673fe58838940ebeb11304c2
ermiry <erick_salas@ermiry.com>
2020-08-14 23:27:49 -0500

    Added methods qualifiers to sprites & textures headers

--- include/cengine/sprites.h
+++ include/cengine/sprites.h
@@ -5,6 +5,7 @@
 
 #include "cengine/types/types.h"
 
+#include "cengine/config.h"
 #include "cengine/renderer.h"
 #include "cengine/graphics.h"
 #include "cengine/textures.h"
@@ -22,11 +23,11 @@ struct _Sprite {
 
 typedef struct _Sprite Sprite;
 
-extern Sprite *sprite_new (void);
+CENGINE_PUBLIC Sprite *sprite_new (void);
 
-extern void sprite_destroy (Sprite *sprite);
+CENGINE_PUBLIC void sprite_destroy (Sprite *sprite);
 
-extern Sprite *sprite_load (const char *filename, Renderer *renderer);
+CENGINE_PUBLIC Sprite *sprite_load (const char *filename, Renderer *renderer);
 
 typedef struct IndividualSprite {
 
@@ -53,14 +54,14 @@ struct _SpriteSheet {
 
 typedef struct _SpriteSheet SpriteSheet;
 
-extern void sprite_sheet_destroy (SpriteSheet *sprite_sheet);
+CENGINE_PUBLIC void sprite_sheet_destroy (SpriteSheet *sprite_sheet);
 
-extern SpriteSheet *sprite_sheet_load (const char *filename, Renderer *renderer);
+CENGINE_PUBLIC SpriteSheet *sprite_sheet_load (const char *filename, Renderer *renderer);
 
-extern void sprite_sheet_set_sprite_size (SpriteSheet *sprite_sheet, u32 w, u32 h);
+CENGINE_PUBLIC void sprite_sheet_set_sprite_size (SpriteSheet *sprite_sheet, u32 w, u32 h);
 
-extern void sprite_sheet_set_scale_factor (SpriteSheet *sprite_sheet, i32 scale_factor);
+CENGINE_PUBLIC void sprite_sheet_set_scale_factor (SpriteSheet *sprite_sheet, i32 scale_factor);
 
-extern void sprite_sheet_crop (SpriteSheet *sprite_sheet);
+CENGINE_PUBLIC void sprite_sheet_crop (SpriteSheet *sprite_sheet);
 
 #endif
--- include/cengine/textures.h
+++ include/cengine/textures.h
@@ -6,6 +6,7 @@
 #include "cengine/types/types.h"
 #include "cengine/types/string.h"
 
+#include "cengine/config.h"
 #include "cengine/graphics.h"
 #include "cengine/renderer.h"
 #include "cengine/sprites.h"
@@ -15,10 +16,10 @@ struct _SpriteSheet;
 
 // solves issue of loading texture in a different thread, see texture load
 // creates a new texture based on a surface
-extern void texture_create_from_surface (Renderer *renderer, SDL_Texture **texture, SDL_Surface *surface);
+CENGINE_PUBLIC void texture_create_from_surface (Renderer *renderer, SDL_Texture **texture, SDL_Surface *surface);
 
 // creates a new texture based on a memory buffer represeinting an image
-extern void texture_create_from_mem_image (Renderer *renderer, SDL_Texture **texture, 
+CENGINE_PUBLIC void texture_create_from_mem_image (Renderer *renderer, SDL_Texture **texture, 
     const void *mem, int mem_size, 
     const char *image_type);
 
@@ -28,29 +29,29 @@ extern void texture_create_from_mem_image (Renderer *renderer, SDL_Texture **tex
 // another thread and you will still get the image data (recovered from the created surface)
 // and the texture will be created in the next render loop, given the illusion of a background charge
 // the image data msut be freed using image_data_delete
-extern ImageData *texture_load (Renderer *renderer, const char *filename, SDL_Texture **texture);
+CENGINE_PUBLIC ImageData *texture_load (Renderer *renderer, const char *filename, SDL_Texture **texture);
 
 // thread safe wrapper method for SDL_UpdateTexture () using SDL_LockTexture () and SDL_UnlockTexture ()
 // mostly intended for internal cengine usage
 // previous texture is not destroyed, surface is destroyed
-extern void texture_update (Renderer *renderer, SDL_Texture **texture, SDL_Surface *surface);
+CENGINE_PUBLIC void texture_update (Renderer *renderer, SDL_Texture **texture, SDL_Surface *surface);
 
 // 27/01/2020 -- 7:11 -- wrapper method for SDL_DestroyTexture () as it is not thread safe
 // this method should be called instead of SDL_DestroyTexture () to avoid video memory leaks
-extern void texture_destroy (Renderer *renderer, SDL_Texture *texture);
+CENGINE_PUBLIC void texture_destroy (Renderer *renderer, SDL_Texture *texture);
 
-extern void texture_destroy_wrapper (void *texture_ptr);
+CENGINE_PUBLIC void texture_destroy_wrapper (void *texture_ptr);
 
 // gets the texture's width and height
-extern void texture_get_dimensions (SDL_Texture *texture, int *w, int *h);
+CENGINE_PUBLIC void texture_get_dimensions (SDL_Texture *texture, int *w, int *h);
 
 #include "cengine/game/camera.h"
 
 struct _Camera;
 
-extern void texture_draw (struct _Camera *cam, Renderer *renderer, struct _Sprite *sprite, i32 x, i32 y, SDL_RendererFlip flip);
+CENGINE_PUBLIC void texture_draw (struct _Camera *cam, Renderer *renderer, struct _Sprite *sprite, i32 x, i32 y, SDL_RendererFlip flip);
 
-extern void texture_draw_frame (struct _Camera *cam, Renderer *renderer, struct _SpriteSheet *spriteSheet, 
+CENGINE_PUBLIC void texture_draw_frame (struct _Camera *cam, Renderer *renderer, struct _SpriteSheet *spriteSheet, 
     i32 x, i32 y, u32 col, u32 row, SDL_RendererFlip flip);
 
 #endif
