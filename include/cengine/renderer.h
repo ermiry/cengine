460c0f4d7afb2051c0c01c2bb332ce3d90862777
ermiry <erick_salas@ermiry.com>
2020-08-14 23:32:48 -0500

    Added method qualifiers to renderer & timer methods

--- include/cengine/renderer.h
+++ include/cengine/renderer.h
@@ -16,8 +16,9 @@
 
 #include "cengine/threads/thread.h"
 
-#include "cengine/video.h"
+#include "cengine/config.h"
 #include "cengine/graphics.h"
+#include "cengine/video.h"
 #include "cengine/window.h"
 
 #include "cengine/ui/ui.h"
@@ -39,9 +40,9 @@ typedef struct SurfaceTexture {
 
 } SurfaceTexture;
 
-extern SurfaceTexture *surface_texture_new (SDL_Surface *surface, SDL_Texture **texture, bool update);
+CENGINE_PRIVATE SurfaceTexture *surface_texture_new (SDL_Surface *surface, SDL_Texture **texture, bool update);
 
-extern void surface_texture_delete (void *st_ptr);
+CENGINE_PRIVATE void surface_texture_delete (void *st_ptr);
 
 /*** Renderer ***/
 
@@ -76,52 +77,52 @@ struct _Renderer {
 
 typedef struct _Renderer Renderer;
 
-extern DoubleList *renderers;
+CENGINE_PRIVATE DoubleList *renderers;
 
-extern void renderer_delete (void *ptr);
+CENGINE_PRIVATE void renderer_delete (void *ptr);
 
 // gets the renderer by its name
-extern Renderer *renderer_get_by_name (const char *name);
+CENGINE_PUBLIC Renderer *renderer_get_by_name (const char *name);
 
 // creates a new empty renderer without a window attached to it
-extern Renderer *renderer_create_empty (const char *name, int display_idx);
+CENGINE_PUBLIC Renderer *renderer_create_empty (const char *name, int display_idx);
 
 // creates a new renderer with a window attached to it
-extern Renderer *renderer_create_with_window (const char *name, int display_idx,
+CENGINE_PUBLIC Renderer *renderer_create_with_window (const char *name, int display_idx,
     Uint32 render_flags,
     const char *window_title, WindowSize window_size, Uint32 window_flags);
 
 // attaches a new window to a renderer
 // creates a new window and then a new render (SDL_Renderer) for it
 // retunrs 0 on success, 1 on error
-extern int renderer_window_attach (Renderer *renderer, Uint32 render_flags, int display_idx,
+CENGINE_PUBLIC int renderer_window_attach (Renderer *renderer, Uint32 render_flags, int display_idx,
     const char *window_title, WindowSize window_size, Uint32 window_flags);
 
-extern void renderer_load_queue_push (Renderer *renderer, SurfaceTexture *st);
+CENGINE_PRIVATE void renderer_load_queue_push (Renderer *renderer, SurfaceTexture *st);
 
-extern void renderer_destroy_queue_push (Renderer *renderer, SDL_Texture *texture);
+CENGINE_PRIVATE void renderer_destroy_queue_push (Renderer *renderer, SDL_Texture *texture);
 
 // sets how many textures the renderer can create in the background every loop
 // example: if you have a frame rate of 30, the default loading factor is 1,
 // so you will load 30 textures in a second, 1 for each frame
-extern void renderer_set_background_texture_loading_factor (Renderer *renderer, u32 bg_loading_factor);
+CENGINE_EXPORT void renderer_set_background_texture_loading_factor (Renderer *renderer, u32 bg_loading_factor);
 
 // sets how many textures the renderer can destroy in the background every loop
-extern void renderer_set_background_texture_destroying_factor (Renderer *renderer, u32 bg_destroying_factor);
+CENGINE_EXPORT void renderer_set_background_texture_destroying_factor (Renderer *renderer, u32 bg_destroying_factor);
 
 // sets an action to executed on every renderer update
 // you can use this if you want to perform action son ui elements, like checking for 
 // the current ui element under the mouse using the ui_element_hover in UI
-extern void renderer_set_update (Renderer *renderer, Action update, void *update_args);
+CENGINE_EXPORT void renderer_set_update (Renderer *renderer, Action update, void *update_args);
 
 // sets the renderer's viewport to be of the specified size
-extern void renderer_set_viewport (Renderer *renderer, u32 x, u32 y, u32 width, u32 height);
+CENGINE_EXPORT void renderer_set_viewport (Renderer *renderer, u32 x, u32 y, u32 width, u32 height);
 
 // sets the renderer's viewport to the previous size
-extern void renderer_set_viewport_to_previous_size (Renderer *renderer);
+CENGINE_EXPORT void renderer_set_viewport_to_previous_size (Renderer *renderer);
 
 // sets the renderer's viewport to be the size of the window
-extern void renderer_set_viewport_to_window_size (Renderer *renderer);
+CENGINE_EXPORT void renderer_set_viewport_to_window_size (Renderer *renderer);
 
 /*** Layers ***/
 
@@ -135,149 +136,149 @@ typedef struct Layer {
 
 extern DoubleList *gos_layers;              // render layers for the gameobjects
 
-extern Layer *layer_get_by_pos (DoubleList *layers, int pos);
+CENGINE_PUBLIC Layer *layer_get_by_pos (DoubleList *layers, int pos);
 
-extern Layer *layer_get_by_name (DoubleList *layers, const char *name);
+CENGINE_PUBLIC Layer *layer_get_by_name (DoubleList *layers, const char *name);
 
 // creates a new layer; 
 // takes the layer name and the layer pos, -1 for last layer
 // pos 0 renders first
 // returns 0 on success, 1 on error
-extern int layer_create (DoubleList *layers, const char *name, int pos, bool gos);
+CENGINE_PUBLIC int layer_create (DoubleList *layers, const char *name, int pos, bool gos);
 
 // adds an element to a layer
 // returns 0 on succes, 1 on error
-extern int layer_add_element (Layer *layer, void *ptr);
+CENGINE_PUBLIC int layer_add_element (Layer *layer, void *ptr);
 
 // adds an element to a layer that is gotten by its name 
 // returns 0 on succes, 1 on error
-extern int layer_add_element_by_name (DoubleList *layers, const char *layer_name, void *ptr);
+CENGINE_PUBLIC int layer_add_element_by_name (DoubleList *layers, const char *layer_name, void *ptr);
 
 // removes an element from a layer
 // returns 0 on succes, 1 on error
-extern int layer_remove_element (Layer *layer, void *ptr);
+CENGINE_PUBLIC int layer_remove_element (Layer *layer, void *ptr);
 
 // removes an element from a layer taht is gotten by its name
 // returns 0 on succes, 1 on error
-extern int layer_remove_element_by_name (DoubleList *layers, const char *layer_name, void *ptr);
+CENGINE_PUBLIC int layer_remove_element_by_name (DoubleList *layers, const char *layer_name, void *ptr);
 
 // init the ui elements layers
-extern DoubleList *ui_layers_init (void);
+CENGINE_PRIVATE DoubleList *ui_layers_init (void);
 
 /*** Surfaces ***/
 
 // creates a new empty surface
-extern SDL_Surface *surface_create (int width, int height);
+CENGINE_PUBLIC SDL_Surface *surface_create (int width, int height);
 
 // loads an image into a new surface
-extern SDL_Surface *surface_load_image (const char *filename);
+CENGINE_PUBLIC SDL_Surface *surface_load_image (const char *filename);
 
 // wrapper function to destroy a sdl surface
-extern void surface_delete (SDL_Surface *surface);
+CENGINE_PUBLIC void surface_delete (SDL_Surface *surface);
 
 /*** Render Basic ***/
 
 // draws a pixel in currently set color
 // returns 0 on success, -1 on error
-extern int render_basic_pixel (Renderer *renderer, int x, int y);
+CENGINE_EXPORT int render_basic_pixel (Renderer *renderer, int x, int y);
 
 // draws a pixel with blending enabled if a < 255
 // returns 0 on success, -1 on failure
-extern int render_basic_pixel_rgba (Renderer *renderer, 
+CENGINE_EXPORT int render_basic_pixel_rgba (Renderer *renderer, 
     int x, int y,
     u8 r, u8 g, u8 b, u8 a);
 
 // draws a pixel with blending enabled if a < 255
 // the color value of the pixel to draw must be in format 0xRRGGBBAA
 // returns 0 on success, -1 on failure
-extern int render_basic_pixel_color (Renderer *renderer, int x, int y, u32 color);
+CENGINE_EXPORT int render_basic_pixel_color (Renderer *renderer, int x, int y, u32 color);
 
 // renders a dot
-extern void render_basic_dot (Renderer *renderer, int x, int y, SDL_Color color,
+CENGINE_EXPORT void render_basic_dot (Renderer *renderer, int x, int y, SDL_Color color,
     float x_scale, float y_scale);
 
 // renders a horizontal line of dots
-extern void render_basic_dot_line_horizontal (Renderer *renderer, int start, int end, int y, int offset, SDL_Color color,
+CENGINE_EXPORT void render_basic_dot_line_horizontal (Renderer *renderer, int start, int end, int y, int offset, SDL_Color color,
     float x_scale, float y_scale);
 
 // renders a vertical line of dots
-extern void render_basic_dot_line_vertical (Renderer *renderer, int start, int end, int x, int offset, SDL_Color color,
+CENGINE_EXPORT void render_basic_dot_line_vertical (Renderer *renderer, int start, int end, int x, int offset, SDL_Color color,
     float x_scale, float y_scale);
 
 // draws a box (filled rectangle) with blending
 // returns 0 on success, -1 on failure
-extern int render_basic_box (Renderer *renderer, int x1, int y1, int x2, int y2, SDL_Color color);
+CENGINE_EXPORT int render_basic_box (Renderer *renderer, int x1, int y1, int x2, int y2, SDL_Color color);
 
 // renders a filled rect
-extern void render_basic_filled_rect (Renderer *renderer, SDL_Rect *rect, SDL_Color color);
+CENGINE_EXPORT void render_basic_filled_rect (Renderer *renderer, SDL_Rect *rect, SDL_Color color);
 
 // renders an outline rect
 // scale works better with even numbers
-extern void render_basic_outline_rect (Renderer *renderer, SDL_Rect *rect, SDL_Color color, float scale_x, float scale_y);
+CENGINE_EXPORT void render_basic_outline_rect (Renderer *renderer, SDL_Rect *rect, SDL_Color color, float scale_x, float scale_y);
 
 // renders a line
 // scale works better with even numbers
-extern void render_basic_line (Renderer *renderer, int x1, int x2, int y1, int y2, SDL_Color color, float scale_x, float scale_y);
+CENGINE_EXPORT void render_basic_line (Renderer *renderer, int x1, int x2, int y1, int y2, SDL_Color color, float scale_x, float scale_y);
 
 // draws a horizontal line with blending
 // returns 0 on success, -1 on failure
-extern int render_basic_horizontal_line (Renderer *renderer, 
+CENGINE_EXPORT int render_basic_horizontal_line (Renderer *renderer, 
     int x1, int x2, int y,
     SDL_Color color);
 
 // draws a vertical line with blending
 // returns 0 on success, -1 on failure
-extern int render_basic_vertical_line (Renderer *renderer,
+CENGINE_EXPORT int render_basic_vertical_line (Renderer *renderer,
     int x, int y1, int y2,
     SDL_Color color);
 
 /*** Render Complex ***/
 
 // renders a rect with transparency
-extern void render_complex_transparent_rect (Renderer *renderer, SDL_Texture **texture, SDL_Rect *rect, SDL_Color color);
+CENGINE_EXPORT void render_complex_transparent_rect (Renderer *renderer, SDL_Texture **texture, SDL_Rect *rect, SDL_Color color);
 
 // draws and arc with blending
 // returns 0 on success, -1 on error
-extern int render_complex_arc (Renderer *renderer, 
+CENGINE_EXPORT int render_complex_arc (Renderer *renderer, 
     int x, int y, int rad,
     int start, int end,
     SDL_Color color);
 
 // draws rounded-corner rectangle (outline) with blending
 // returns 0 on success, -1 on failure
-extern int render_complex_rounded_rect (Renderer *renderer, 
+CENGINE_EXPORT int render_complex_rounded_rect (Renderer *renderer, 
     int x1, int y1, int x2, int y2, int rad,
     SDL_Color color);
 
 // draws rounded-corner box (filled rectangle) with blending
 // returns 0 on success, -1 on failure
-extern int render_complex_rounded_box (Renderer *renderer, 
+CENGINE_EXPORT int render_complex_rounded_box (Renderer *renderer, 
     int x1, int y1, int x2, int y2, int rad,
     SDL_Color color);
 
 // draws an ellipse or a filled ellipse with blending
 // returns 0 on success, -1 on failure
-extern int render_complex_ellipse (Renderer *renderer,
+CENGINE_EXPORT int render_complex_ellipse (Renderer *renderer,
     int x, int y,
     int rx, int ry,
     SDL_Color color, bool filled);
 
 // draws a circle with blending
 // returns 0 on success, -1 on failure
-extern int render_complex_circle (Renderer *renderer,
+CENGINE_EXPORT int render_complex_circle (Renderer *renderer,
     int x, int y,
     int rad, 
     SDL_Color color, bool filled);
 
 /*** Render ***/
 
-extern void render (Renderer *renderer);
+CENGINE_PRIVATE void render (Renderer *renderer);
 
 /*** Public ***/
 
 // inits cengine render capabilities
-extern u8 render_init (void);
+CENGINE_PRIVATE u8 render_init (void);
 
-extern u8 render_end (void);
+CENGINE_PRIVATE u8 render_end (void);
 
 #endif
--- include/cengine/timer.h
+++ include/cengine/timer.h
@@ -11,33 +11,35 @@
 #include "cengine/types/types.h"
 #include "cengine/types/string.h"
 
+#include "cengine/config.h"
+
 #pragma region time
 
 typedef struct timespec TimeSpec;
 
-extern void timespec_delete (void *timespec_ptr);
+CENGINE_PUBLIC void timespec_delete (void *timespec_ptr);
 
-extern TimeSpec *timer_get_timespec (void);
+CENGINE_PUBLIC TimeSpec *timer_get_timespec (void);
 
-extern double timer_elapsed_time (TimeSpec *start, TimeSpec *end);
+CENGINE_PUBLIC double timer_elapsed_time (TimeSpec *start, TimeSpec *end);
 
-extern void timer_sleep_for_seconds (double seconds);
+CENGINE_PUBLIC void timer_sleep_for_seconds (double seconds);
 
-extern struct tm *timer_get_gmt_time (void);
+CENGINE_PUBLIC struct tm *timer_get_gmt_time (void);
 
-extern struct tm *timer_get_local_time (void);
+CENGINE_PUBLIC struct tm *timer_get_local_time (void);
 
 // returns a string representing the 24h time 
-extern String *timer_time_to_string (struct tm *timeinfo);
+CENGINE_PUBLIC String *timer_time_to_string (struct tm *timeinfo);
 
 // returns a string with day/month/year
-extern String *timer_date_to_string (struct tm *timeinfo);
+CENGINE_PUBLIC String *timer_date_to_string (struct tm *timeinfo);
 
 // returns a string with day/month/year - 24h time
-extern String *timer_date_and_time_to_string (struct tm *timeinfo);
+CENGINE_PUBLIC String *timer_date_and_time_to_string (struct tm *timeinfo);
 
 // returns a string representing the time with custom format
-extern String *timer_time_to_string_custom (struct tm *timeinfo, const char *format);
+CENGINE_PUBLIC String *timer_time_to_string_custom (struct tm *timeinfo, const char *format);
 
 #pragma endregion
 
@@ -55,19 +57,19 @@ typedef struct Timer {
 
 } Timer;
 
-extern Timer *timer_new (void);
+CENGINE_PUBLIC Timer *timer_new (void);
 
-extern void timer_destroy (Timer *timer);
+CENGINE_PUBLIC void timer_destroy (Timer *timer);
 
-extern void timer_start (Timer *timer);
+CENGINE_PUBLIC void timer_start (Timer *timer);
 
-extern void timer_stop (Timer *timer);
+CENGINE_PUBLIC void timer_stop (Timer *timer);
 
-extern void timer_pause (Timer *timer);
+CENGINE_PUBLIC void timer_pause (Timer *timer);
 
-extern void timer_unpause (Timer *timer);
+CENGINE_PUBLIC void timer_unpause (Timer *timer);
 
-extern u32 timer_get_ticks (Timer *timer);
+CENGINE_PUBLIC u32 timer_get_ticks (Timer *timer);
 
 #pragma endregion
 
