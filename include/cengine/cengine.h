1c8561f4348b27ccd9db8a72a5f5377d3dfdbeeb
ermiry <erick_salas@ermiry.com>
2020-08-14 23:21:53 -0500

    Refactor main cengine header

--- include/cengine/cengine.h
+++ include/cengine/cengine.h
@@ -3,9 +3,12 @@
 
 #include <stdbool.h>
 
+#include "cengine/config.h"
 #include "cengine/renderer.h"
 
 #include "cengine/ui/textbox.h"
+
+#define EXIT_FAILURE    1
 
 #define MIN(a, b) ((a) < (b) ? (a) : (b))
 #define MAX(a, b) ((a) > (b) ? (a) : (b))
@@ -13,31 +16,31 @@
 /*** cengine ***/
 
 // call this to initialize cengine
-extern int cengine_init (void);
+CENGINE_EXPORT int cengine_init (void);
 
 // call this when you want to exit cengine 
-extern int cengine_end (void);
+CENGINE_EXPORT int cengine_end (void);
 
 // call this when you want to start cengine's main loops (update & render)
-extern int cengine_start (int fps);
+CENGINE_EXPORT int cengine_start (int fps);
 
 /*** threads ***/
 
-extern bool running;
+CENGINE_PUBLIC bool running;
 
-extern unsigned int fps_limit;
+CENGINE_EXPORT unsigned int fps_limit;
 
-extern unsigned int cengine_get_fps_limit (void);
+CENGINE_EXPORT unsigned int cengine_get_fps_limit (void);
 
-extern void cengine_set_main_fps_text (TextBox *text);
+CENGINE_EXPORT void cengine_set_main_fps_text (TextBox *text);
 
-extern void cengine_set_update_fps_text (TextBox *text);
+CENGINE_EXPORT void cengine_set_update_fps_text (TextBox *text);
 
 /*** other ***/
 
-extern void (*cengine_quit)(void);
+CENGINE_PUBLIC void (*cengine_quit)(void);
 
 // sets the function to be executed on SDL_QUIT event
-extern void cengine_set_quit (void (*quit)(void));
+CENGINE_EXPORT void cengine_set_quit (void (*quit)(void));
 
 #endif
