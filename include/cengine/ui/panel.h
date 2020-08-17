d740b8ba493e479651e3894478d4bae39d99cf5d
ermiry <erick_salas@ermiry.com>
2020-08-16 23:52:04 -0500

    Updated panel header with method qualifiers

--- include/cengine/ui/panel.h
+++ include/cengine/ui/panel.h
@@ -7,8 +7,9 @@
 #include "cengine/types/types.h"
 #include "cengine/collections/dlist.h"
 
-#include "cengine/video.h"
+#include "cengine/config.h"
 #include "cengine/renderer.h"
+#include "cengine/video.h"
 
 #include "cengine/ui/ui.h"
 #include "cengine/ui/components/transform.h"
@@ -51,71 +52,71 @@ struct _Panel {
 
 typedef struct _Panel Panel;
 
-extern void ui_panel_delete (void *panel_ptr);
+CENGINE_PUBLIC void ui_panel_delete (void *panel_ptr);
 
 // sets the panel's UI position
-extern void ui_panel_set_pos (Panel *panel, UIRect *ref_rect, UIPosition pos, Renderer *renderer);
+CENGINE_EXPORT void ui_panel_set_pos (Panel *panel, UIRect *ref_rect, UIPosition pos, Renderer *renderer);
 
 // sets the panel's UI position offset
-extern void ui_panel_set_pos_offset (Panel *panel, int x_offset, int y_offset);
+CENGINE_EXPORT void ui_panel_set_pos_offset (Panel *panel, int x_offset, int y_offset);
 
 // updates one panel's child position
-extern void ui_panel_child_update_pos (Panel *panel, UIElement *child);
+CENGINE_EXPORT void ui_panel_child_update_pos (Panel *panel, UIElement *child);
 
 // updates the panel's children positions
-extern void ui_panel_children_update_pos (Panel *panel);
+CENGINE_EXPORT void ui_panel_children_update_pos (Panel *panel);
 
 // sets the background colour of the panel
-extern void ui_panel_set_bg_colour (Panel *panel, Renderer *renderer, RGBA_Color colour);
+CENGINE_EXPORT void ui_panel_set_bg_colour (Panel *panel, Renderer *renderer, RGBA_Color colour);
 
 // removes the background from the panel
-extern void ui_panel_remove_background (Panel *panel);
+CENGINE_EXPORT void ui_panel_remove_background (Panel *panel);
 
 // sets the panel's outline colour
-extern void ui_panel_set_ouline_colour (Panel *panel, RGBA_Color colour);
+CENGINE_EXPORT void ui_panel_set_ouline_colour (Panel *panel, RGBA_Color colour);
 
 // sets the panel's outline scale
-extern void ui_panel_set_ouline_scale (Panel *panel, float x_scale, float y_scale);
+CENGINE_EXPORT void ui_panel_set_ouline_scale (Panel *panel, float x_scale, float y_scale);
 
 // removes the ouline form the panel
-extern void ui_panel_remove_outline (Panel *panel);
+CENGINE_EXPORT void ui_panel_remove_outline (Panel *panel);
 
 // get the amount of elements that are inside the panel's layout
-extern size_t ui_panel_layout_get_elements_count (Panel *panel);
+CENGINE_EXPORT size_t ui_panel_layout_get_elements_count (Panel *panel);
 
 // sets the layout for the panel
-extern void ui_panel_layout_set (Panel *panel, LayoutType type, struct _Renderer *renderer);
+CENGINE_EXPORT void ui_panel_layout_set (Panel *panel, LayoutType type, struct _Renderer *renderer);
 
 // removes the existing layout form the panel
-extern void ui_panel_layout_remove (Panel *panel);
+CENGINE_EXPORT void ui_panel_layout_remove (Panel *panel);
 
 // adds a new ui element to the panel's layout's in the specified position (0 indexed)
-extern void ui_panel_layout_add_element_at_pos (Panel *panel, UIElement *ui_element, u32 pos);
+CENGINE_EXPORT void ui_panel_layout_add_element_at_pos (Panel *panel, UIElement *ui_element, u32 pos);
 
 // adds a new ui element to the panel's layout's END
-extern void ui_panel_layout_add_element_at_end (Panel *panel, UIElement *ui_element);
+CENGINE_EXPORT void ui_panel_layout_add_element_at_end (Panel *panel, UIElement *ui_element);
 
  // returns the ui element that is at the required position in the panel's layout
-extern UIElement *ui_panel_layout_get_element_at (Panel *panel, unsigned int pos);
+CENGINE_EXPORT UIElement *ui_panel_layout_get_element_at (Panel *panel, unsigned int pos);
 
 // removes a ui element form the panel layout
-extern u8 ui_panel_layout_remove_element (Panel *panel, UIElement *ui_element);
+CENGINE_EXPORT u8 ui_panel_layout_remove_element (Panel *panel, UIElement *ui_element);
 
 // adds a new child to the panel
-extern void ui_panel_child_add (Panel *panel, UIElement *ui_element);
+CENGINE_EXPORT void ui_panel_child_add (Panel *panel, UIElement *ui_element);
 
 // removes a child from the panel (the dlist uses a ui element ids comparator)
 // returns the ui element that was removed
-extern UIElement *ui_panel_child_remove (Panel *panel, UIElement *ui_element);
+CENGINE_EXPORT UIElement *ui_panel_child_remove (Panel *panel, UIElement *ui_element);
 
 // creates a new panel
 // x and y for position
-extern Panel *ui_panel_create (i32 x, i32 y, u32 w, u32 h, UIPosition pos, Renderer *renderer);
+CENGINE_EXPORT Panel *ui_panel_create (i32 x, i32 y, u32 w, u32 h, UIPosition pos, Renderer *renderer);
 
 // rezises the panel based on window size
-extern void ui_panel_resize (Panel *panel, WindowSize window_original_size, WindowSize window_new_size);
+CENGINE_PRIVATE void ui_panel_resize (Panel *panel, WindowSize window_original_size, WindowSize window_new_size);
 
 // draws the panel to the screen
-extern void ui_panel_draw (Panel *panel, Renderer *renderer);
+CENGINE_PRIVATE void ui_panel_draw (Panel *panel, Renderer *renderer);
 
 #endif
