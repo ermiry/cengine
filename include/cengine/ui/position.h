ca387e489c85310e4cd8c0514f68b555e51c5617
ermiry <erick_salas@ermiry.com>
2020-08-16 23:52:35 -0500

    Updated ui position header with method qualifiers

--- include/cengine/ui/position.h
+++ include/cengine/ui/position.h
@@ -3,15 +3,16 @@
 
 #include <stdbool.h>
 
+#include "cengine/config.h"
 #include "cengine/renderer.h"
 
 #include "cengine/ui/types/types.h"
 
 struct _Renderer;
 
-extern void ui_position_update (struct _Renderer *renderer, void *transform_ptr, UIRect *ref_rect, bool offset);
+CENGINE_PUBLIC void ui_position_update (struct _Renderer *renderer, void *transform_ptr, UIRect *ref_rect, bool offset);
 
 // updates the transform's position relative to be relative to its parent (x && y = 0)
-extern void ui_position_update_to_parent (void *parent_transform_ptr, void *transform_ptr, bool offset);
+CENGINE_PUBLIC void ui_position_update_to_parent (void *parent_transform_ptr, void *transform_ptr, bool offset);
 
 #endif
