70badf5a3dc423f7faee6774eeca7afea9a5e5d5
ermiry <erick_salas@ermiry.com>
2020-08-16 23:52:26 -0500

    Updated tooltip header with method qualifiers

--- include/cengine/ui/tooltip.h
+++ include/cengine/ui/tooltip.h
@@ -7,6 +7,7 @@
 
 #include "cengine/collections/dlist.h"
 
+#include "cengine/config.h"
 #include "cengine/renderer.h"
 
 #include "cengine/ui/ui.h"
@@ -36,32 +37,32 @@ typedef struct Tooltip {
 
 } Tooltip;
 
-extern void ui_tooltip_delete (void *tooltip_ptr);
+CENGINE_PUBLIC void ui_tooltip_delete (void *tooltip_ptr);
 
 // sets the background colour of the tooltip
-extern void ui_tooltip_set_bg_colour (Tooltip *tooltip, struct _Renderer *renderer, RGBA_Color colour);
+CENGINE_EXPORT void ui_tooltip_set_bg_colour (Tooltip *tooltip, struct _Renderer *renderer, RGBA_Color colour);
 
 // removes the background from the tooltip
-extern void ui_tooltip_remove_background (Tooltip *tooltip);
+CENGINE_EXPORT void ui_tooltip_remove_background (Tooltip *tooltip);
 
 // updates the tooltip's children positions
-extern void ui_tooltip_children_update_pos (Tooltip *tooltip);
+CENGINE_EXPORT void ui_tooltip_children_update_pos (Tooltip *tooltip);
 
 // adds a new ui element to the tooltip's layout's in the specified position (0 indexed)
-extern void ui_tooltip_add_element_at_pos (Tooltip *tooltip, struct _UIElement *ui_element, u32 pos);
+CENGINE_EXPORT void ui_tooltip_add_element_at_pos (Tooltip *tooltip, struct _UIElement *ui_element, u32 pos);
 
 // adds a new ui element to the tooltip's layout's END
-extern void ui_tooltip_add_element_at_end (Tooltip *tooltip, struct _UIElement *ui_element);
+CENGINE_EXPORT void ui_tooltip_add_element_at_end (Tooltip *tooltip, struct _UIElement *ui_element);
 
 // returns the ui element that is at the required position in the tooltip's layout
-extern struct _UIElement *ui_tooltip_get_element_at (Tooltip *tooltip, unsigned int pos);
+CENGINE_EXPORT struct _UIElement *ui_tooltip_get_element_at (Tooltip *tooltip, unsigned int pos);
 
 // removes a ui element form the tooltip's layout
-extern u8 ui_tooltip_remove_element (Tooltip *tooltip, struct _UIElement *ui_element);
+CENGINE_EXPORT u8 ui_tooltip_remove_element (Tooltip *tooltip, struct _UIElement *ui_element);
 
-extern Tooltip *ui_tooltip_create (u32 w, u32 h, struct _Renderer *renderer);
+CENGINE_EXPORT Tooltip *ui_tooltip_create (u32 w, u32 h, struct _Renderer *renderer);
 
 // draws the tooltip to the screen
-extern void ui_tooltip_draw (Tooltip *tooltip, struct _Renderer *renderer);
+CENGINE_PRIVATE void ui_tooltip_draw (Tooltip *tooltip, struct _Renderer *renderer);
 
 #endif
