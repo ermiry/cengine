0a11ef4224ddb509e1828fbff9549eb69d53b8f8
ermiry <erick_salas@ermiry.com>
2020-08-16 23:51:42 -0500

    Updated font header with method qualifiers

--- include/cengine/ui/font.h
+++ include/cengine/ui/font.h
@@ -7,6 +7,7 @@
 #include "cengine/types/types.h"
 #include "cengine/types/string.h"
 
+#include "cengine/config.h"
 #include "cengine/renderer.h"
 
 #include "cengine/ui/ui.h"
@@ -56,40 +57,40 @@ typedef struct Font {
 
 } Font;
 
-extern void ui_font_delete (void *font_ptr);;
+CENGINE_PUBLIC void ui_font_delete (void *font_ptr);;
 
-extern FontSource *font_source_get_by_size (Font *font, unsigned int size);
+CENGINE_PUBLIC FontSource *font_source_get_by_size (Font *font, unsigned int size);
 
 // creates a new font structure that requires a name for the font and the file of the font
 // the font is added to internal cengine structures and should not be deleted
-extern Font *ui_font_create (const char *font_name, const char *font_filename);
+CENGINE_EXPORT Font *ui_font_create (const char *font_name, const char *font_filename);
 
 // sets the font sizes to be loaded
 // returns 0 on success, 1 on error
-extern u8 ui_font_set_sizes (Font *font, u8 n_sizes, ...);
+CENGINE_EXPORT u8 ui_font_set_sizes (Font *font, u8 n_sizes, ...);
 
 // loads the font with the specified values
 // returns 0 on success, 1 on error
-extern u8 ui_font_load (Font *font, struct _Renderer *renderer, int style);
+CENGINE_EXPORT u8 ui_font_load (Font *font, struct _Renderer *renderer, int style);
 
 // gets a refrence to the default font --> the first one that was added
-extern Font *ui_font_get_default (void);
+CENGINE_EXPORT Font *ui_font_get_default (void);
 
 // gets a refrence to a ui font by its name
-extern Font *ui_font_get_by_name (const char *name);
+CENGINE_EXPORT Font *ui_font_get_by_name (const char *name);
 
 /*** Misc ***/
 
-extern u32 get_code_point_from_UTF8 (const char **c, u8 advancePtr);
+CENGINE_PRIVATE u32 get_code_point_from_UTF8 (const char **c, u8 advancePtr);
 
 /*** Main ***/
 
 // called by internal cengine methods
 // inits cengine ui font capabilities
-extern u8 ui_fonts_init (void);
+CENGINE_PRIVATE u8 ui_fonts_init (void);
 
 // called by internal cengine methods
 // destroys any cengine font data left
-extern void ui_font_end (void);
+CENGINE_PRIVATE void ui_font_end (void);
 
 #endif
