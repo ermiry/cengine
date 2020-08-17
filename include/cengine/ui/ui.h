3b1e934b50dc52cd4733ef4efa332857ade10d8f
ermiry <erick_salas@ermiry.com>
2020-08-16 23:51:03 -0500

    Added method qualifiers to main ui header

--- include/cengine/ui/ui.h
+++ include/cengine/ui/ui.h
@@ -6,8 +6,9 @@
 #include "cengine/types/types.h"
 #include "cengine/collections/dlist.h"
 
-#include "cengine/window.h"
+#include "cengine/config.h"
 #include "cengine/renderer.h"
+#include "cengine/window.h"
 
 #include "cengine/ui/components/transform.h"
 
@@ -58,28 +59,28 @@ struct _UIElement {
 typedef struct _UIElement UIElement;
 
 // ui element constructor
-extern UIElement *ui_element_create (struct _UI *ui, UIElementType type);
+CENGINE_PUBLIC UIElement *ui_element_create (struct _UI *ui, UIElementType type);
 
 // deletes the ui element directly
-extern void ui_element_delete (void *ui_element_ptr);
+CENGINE_PUBLIC void ui_element_delete (void *ui_element_ptr);
 
-extern void ui_element_delete_dummy (void *ui_element_ptr);
+CENGINE_PUBLIC void ui_element_delete_dummy (void *ui_element_ptr);
 
 // removes the element from the ui and then deletes it
-extern void ui_element_destroy (void *ui_element_ptr);
+CENGINE_PUBLIC void ui_element_destroy (void *ui_element_ptr);
 
-extern int ui_element_comparator (const void *one, const void *two);
+CENGINE_PUBLIC int ui_element_comparator (const void *one, const void *two);
 
 // sets the render layer of the ui element
 // removes it from the one it is now and adds it to the new one
 // returns 0 on success, 1 on error
-extern int ui_element_set_layer (struct _UI *ui, UIElement *ui_element, const char *layer_name);
+CENGINE_PUBLIC int ui_element_set_layer (struct _UI *ui, UIElement *ui_element, const char *layer_name);
 
-extern void ui_element_toggle_active (UIElement *ui_element);
+CENGINE_PUBLIC void ui_element_toggle_active (UIElement *ui_element);
 
-extern void ui_element_set_active (UIElement *ui_element, bool active);
+CENGINE_PUBLIC void ui_element_set_active (UIElement *ui_element, bool active);
 
-extern void parent_ui_element_get_position (UIElement *parent, int *x, int *y);
+CENGINE_PUBLIC void parent_ui_element_get_position (UIElement *parent, int *x, int *y);
 
 struct _UI {
 
@@ -95,37 +96,37 @@ struct _UI {
 
 typedef struct _UI UI;
 
-extern void ui_delete (void *ui_ptr);
+CENGINE_PRIVATE void ui_delete (void *ui_ptr);
 
 // init our ui elements structures
-extern UI *ui_create (void);
+CENGINE_PRIVATE UI *ui_create (void);
 
-extern UIElement *ui_element_hover_get (UI *ui);
+CENGINE_PRIVATE UIElement *ui_element_hover_get (UI *ui);
 
 // adds a new ui element back to the UI
 // returns 0 on success, 1 on error
-extern u8 ui_add_element (UI *ui, UIElement *ui_element);
+CENGINE_PUBLIC u8 ui_add_element (UI *ui, UIElement *ui_element);
 
 // removes a ui element from the UI
-extern UIElement *ui_remove_element (UI *ui, UIElement *ui_element);
+CENGINE_PUBLIC UIElement *ui_remove_element (UI *ui, UIElement *ui_element);
 
 /*** render ***/
 
 // resize the ui elements to fit new window
-extern void ui_resize (struct _Window *window);
+CENGINE_PRIVATE void ui_resize (struct _Window *window);
 
 // renders the ui element to the screen
-extern void ui_render_element (struct _Renderer *renderer, UIElement *ui_element);
+CENGINE_PRIVATE void ui_render_element (struct _Renderer *renderer, UIElement *ui_element);
 
 // renders all the current active ui to the screen
-extern void ui_render (struct _Renderer *renderer);
+CENGINE_PRIVATE void ui_render (struct _Renderer *renderer);
 
 /*** main ***/
 
 // initializes cengine's ui capabilities
-extern u8 ui_init (void);
+CENGINE_PRIVATE u8 ui_init (void);
 
 // destroy common ui elements
-extern u8 ui_end (void);
+CENGINE_PRIVATE u8 ui_end (void);
 
 #endif
