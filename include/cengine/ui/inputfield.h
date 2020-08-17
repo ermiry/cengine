aac5931ae50f6d1ede801c468e01c34dbb5aa448
ermiry <erick_salas@ermiry.com>
2020-08-16 23:51:53 -0500

    Updated input header with method qualifiers

--- include/cengine/ui/inputfield.h
+++ include/cengine/ui/inputfield.h
@@ -9,6 +9,7 @@
 #include "cengine/types/types.h"
 #include "cengine/types/string.h"
 
+#include "cengine/config.h"
 #include "cengine/renderer.h"
 
 #include "cengine/ui/types/types.h"
@@ -57,90 +58,90 @@ typedef struct InputField {
 
 } InputField;
 
-extern void ui_input_field_delete (void *input_ptr);
+CENGINE_PUBLIC void ui_input_field_delete (void *input_ptr);
 
 // sets the input field to be active depending on values
-extern void ui_input_field_set_active (InputField *input, bool active);
+CENGINE_EXPORT void ui_input_field_set_active (InputField *input, bool active);
 
 // toggles the input field to be active or not
-extern void ui_input_field_toggle_active (InputField *input);
+CENGINE_EXPORT void ui_input_field_toggle_active (InputField *input);
 
 // sets the inputs's UI position
-extern void ui_input_field_set_pos (InputField *input, UIRect *ref_rect, UIPosition pos, struct _Renderer *renderer);
+CENGINE_EXPORT void ui_input_field_set_pos (InputField *input, UIRect *ref_rect, UIPosition pos, struct _Renderer *renderer);
 
 // sets the input's position offset
-extern void ui_input_set_pos_offset (InputField *input, int x_offset, int y_offset);
+CENGINE_EXPORT void ui_input_set_pos_offset (InputField *input, int x_offset, int y_offset);
 
 // sets the input placeholder text
-extern void ui_input_field_placeholder_text_set (InputField *input, struct _Renderer *renderer, const char *text,
+CENGINE_EXPORT void ui_input_field_placeholder_text_set (InputField *input, struct _Renderer *renderer, const char *text,
 	Font *font, u32 size, RGBA_Color text_color);
 
 // updates the input's placeholder text
-extern void ui_input_field_placeholder_text_update (InputField *input, struct _Renderer *renderer, const char *text);
+CENGINE_EXPORT void ui_input_field_placeholder_text_update (InputField *input, struct _Renderer *renderer, const char *text);
 
 // sets the input placeholder text position
-extern void ui_input_field_placeholder_text_pos_set (InputField *input, UIPosition pos);
+CENGINE_EXPORT void ui_input_field_placeholder_text_pos_set (InputField *input, UIPosition pos);
 
 // sets the input placeholder text position offset
-extern void ui_input_field_placeholder_text_pos_offset_set (InputField *input, int x_offset, int y_offset);
+CENGINE_EXPORT void ui_input_field_placeholder_text_pos_offset_set (InputField *input, int x_offset, int y_offset);
 
 // updates the input's placeholder text position
-extern void ui_input_field_placeholder_text_pos_update (InputField *input);
+CENGINE_EXPORT void ui_input_field_placeholder_text_pos_update (InputField *input);
 
 // sets the input field's text
-extern void ui_input_field_text_set (InputField *input, struct _Renderer *renderer, const char *text,
+CENGINE_EXPORT void ui_input_field_text_set (InputField *input, struct _Renderer *renderer, const char *text,
 	Font *font, u32 size, RGBA_Color text_color, bool is_password);
 
 // updates the placeholder text (redraws the text component)
-extern void ui_input_field_text_update (InputField *input, struct _Renderer *renderer, const char *update_text);
+CENGINE_EXPORT void ui_input_field_text_update (InputField *input, struct _Renderer *renderer, const char *update_text);
 
 // sets the input field's text position
-extern void ui_input_field_text_pos_set (InputField *input, UIPosition pos);
+CENGINE_EXPORT void ui_input_field_text_pos_set (InputField *input, UIPosition pos);
 
 // sets the input text position offset
-extern void ui_input_field_text_pos_offset_set (InputField *input, int x_offset, int y_offset);
+CENGINE_EXPORT void ui_input_field_text_pos_offset_set (InputField *input, int x_offset, int y_offset);
 
 // updates the input's text position
-extern void ui_input_field_text_pos_update (InputField *input);
+CENGINE_EXPORT void ui_input_field_text_pos_update (InputField *input);
 
 // sets the input field's text color
-extern void ui_input_field_text_color_set (InputField *input, struct _Renderer *renderer, RGBA_Color color);
+CENGINE_EXPORT void ui_input_field_text_color_set (InputField *input, struct _Renderer *renderer, RGBA_Color color);
 
 // returns the current input text
-extern String *ui_input_field_input_get (InputField *input);
+CENGINE_EXPORT String *ui_input_field_input_get (InputField *input);
 
 // returns the actual password value
-extern String *ui_input_field_password_get (InputField *input);
+CENGINE_EXPORT String *ui_input_field_password_get (InputField *input);
 
 // sets the input field's outline colour
-extern void ui_input_field_ouline_set_colour (InputField *input, RGBA_Color colour);
+CENGINE_EXPORT void ui_input_field_ouline_set_colour (InputField *input, RGBA_Color colour);
 
 // sets the input field's outline scale
-extern void ui_input_field_ouline_set_scale (InputField *input, float x_scale, float y_scale);
+CENGINE_EXPORT void ui_input_field_ouline_set_scale (InputField *input, float x_scale, float y_scale);
 
 // removes the ouline form the input field
-extern void ui_input_field_outline_remove (InputField *input);
+CENGINE_EXPORT void ui_input_field_outline_remove (InputField *input);
 
 // sets the input field's background color
-extern void ui_input_field_bg_color_set (InputField *input, struct _Renderer *renderer, RGBA_Color color);
+CENGINE_EXPORT void ui_input_field_bg_color_set (InputField *input, struct _Renderer *renderer, RGBA_Color color);
 
 // removes the background from the input field
-extern void ui_input_field_bg_remove (InputField *input);
+CENGINE_EXPORT void ui_input_field_bg_remove (InputField *input);
 
 // sets the option to draw an outline rect when the input is selected
-extern void ui_input_field_selected_set (InputField *input, RGBA_Color selected_color);
+CENGINE_EXPORT void ui_input_field_selected_set (InputField *input, RGBA_Color selected_color);
 
 // sets an action to be triggered every input
 // works for every keystroke, paste, delete
-extern void ui_input_field_set_on_key_input (InputField *input, Action on_key_input, void *on_key_input_args);
+CENGINE_EXPORT void ui_input_field_set_on_key_input (InputField *input, Action on_key_input, void *on_key_input_args);
 
 // creates a new input field
-extern InputField *ui_input_field_create (i32 x, i32 y, u32 w, u32 h, UIPosition pos, struct _Renderer *renderer);
+CENGINE_EXPORT InputField *ui_input_field_create (i32 x, i32 y, u32 w, u32 h, UIPosition pos, struct _Renderer *renderer);
 
 // draws the input field
-extern void ui_input_field_draw (InputField *input, struct _Renderer *renderer);
+CENGINE_PRIVATE void ui_input_field_draw (InputField *input, struct _Renderer *renderer);
 
 // updates the input field with the correct values
-extern void ui_input_field_update (InputField *input, struct _Renderer *renderer);
+CENGINE_PRIVATE void ui_input_field_update (InputField *input, struct _Renderer *renderer);
 
 #endif
