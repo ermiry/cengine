1f8472b69e58afdcacff7e65ec2d42d2597d7300
ermiry <erick_salas@ermiry.com>
2020-08-20 23:14:37 -0500

    Updated grid layout header with method qualifiers

--- include/cengine/ui/layout/grid.h
+++ include/cengine/ui/layout/grid.h
@@ -71,84 +71,84 @@ typedef struct GridLayout {
 
 } GridLayout;
 
-extern void ui_layout_grid_delete (void *grid_ptr);
+CENGINE_PRIVATE void ui_layout_grid_delete (void *grid_ptr);
 
 // get the amount of elements that are inside the grid
-extern size_t ui_layout_grid_get_elements_count (GridLayout *grid);
+CENGINE_PUBLIC size_t ui_layout_grid_get_elements_count (GridLayout *grid);
 
 // get the current grid's size, cols and rows
-extern void ui_layout_grid_get_size (GridLayout *grid, u32 *cols, u32 *rows);
+CENGINE_PUBLIC void ui_layout_grid_get_size (GridLayout *grid, u32 *cols, u32 *rows);
 
 // sets the number of columns and rows available for the grid
-extern void ui_layout_grid_set_grid (GridLayout *grid, u32 cols, u32 rows);
+CENGINE_PUBLIC void ui_layout_grid_set_grid (GridLayout *grid, u32 cols, u32 rows);
 
 // sets the number of elements x row
-extern void ui_layout_grid_set_elements_x_row (GridLayout *grid, u32 n_elements_x_row);
+CENGINE_PUBLIC void ui_layout_grid_set_elements_x_row (GridLayout *grid, u32 n_elements_x_row);
 
 // sets the number of elements x col
-extern void ui_layout_grid_set_elements_x_col (GridLayout *grid, u32 n_elements_x_col);
+CENGINE_PUBLIC void ui_layout_grid_set_elements_x_col (GridLayout *grid, u32 n_elements_x_col);
 
 // sets the preferred size of the cells, the layout must be of the correct size
-extern void ui_layout_grid_set_cell_size (GridLayout *grid, u32 width, u32 height);
+CENGINE_PUBLIC void ui_layout_grid_set_cell_size (GridLayout *grid, u32 width, u32 height);
 
 // sets the cell padding - distance between cells
-extern void ui_layout_grid_set_cell_padding (GridLayout *grid, u32 cell_padding_x, u32 cell_padding_y);
+CENGINE_PUBLIC void ui_layout_grid_set_cell_padding (GridLayout *grid, u32 cell_padding_x, u32 cell_padding_y);
 
 // sets the cell's inner padding percentage
-extern void ui_layout_grid_set_cell_inner_padding (GridLayout *grid, 
+CENGINE_PUBLIC void ui_layout_grid_set_cell_inner_padding (GridLayout *grid, 
     float cell_x_inner_padding_percentage, float cell_y_inner_padding_percentage);
 
 // sets the ui position to be used for grid element's, by the default it is UI_POS_MIDDLE_CENTER
-extern void ui_layout_grid_set_cell_position (GridLayout *grid, UIPosition pos);
+CENGINE_PUBLIC void ui_layout_grid_set_cell_position (GridLayout *grid, UIPosition pos);
 
 // sets the alignment to use in the x axis
-extern void ui_layout_grid_set_x_axis_alignment (GridLayout *grid, AxisAlignment align);
+CENGINE_PUBLIC void ui_layout_grid_set_x_axis_alignment (GridLayout *grid, AxisAlignment align);
 
 // sets the alignment to use in the y axis
-extern void ui_layout_grid_set_y_axis_alignment (GridLayout *grid, AxisAlignment align);
+CENGINE_PUBLIC void ui_layout_grid_set_y_axis_alignment (GridLayout *grid, AxisAlignment align);
 
 // enables / disbale scrolling in the grid layout
-extern void ui_layout_grid_toggle_scrolling (GridLayout *grid, bool enable);
+CENGINE_PUBLIC void ui_layout_grid_toggle_scrolling (GridLayout *grid, bool enable);
 
 // sets how fast the elements move when scrolling, the default is a value of 5
-extern void ui_layout_grid_set_scroll_sensitivity (GridLayout *grid, u32 sensitivity);
+CENGINE_PUBLIC void ui_layout_grid_set_scroll_sensitivity (GridLayout *grid, u32 sensitivity);
 
 // creates a new grid layout
 // x and y for position, w and h for dimensions
-extern GridLayout *ui_layout_grid_create (i32 x, i32 y, u32 w, u32 h, struct _Renderer *renderer);
+CENGINE_PUBLIC GridLayout *ui_layout_grid_create (i32 x, i32 y, u32 w, u32 h, struct _Renderer *renderer);
 
 // updates the grid with a new size (cols and rows)
 // returns 0 on success update, 1 on failure
-extern int ui_layout_grid_update_size (GridLayout *grid, u32 cols, u32 cell_width, u32 cell_height);
+CENGINE_PUBLIC int ui_layout_grid_update_size (GridLayout *grid, u32 cols, u32 cell_width, u32 cell_height);
 
 // adds a new element to the grid in the specified pos (0 indexed)
 // if its greater than the current size, it will be added at the end
 // returns 0 on success, 1 if failed to add
-extern u8 ui_layout_grid_add_element_at_pos (GridLayout *grid, UIElement *ui_element, u32 pos);
+CENGINE_PUBLIC u8 ui_layout_grid_add_element_at_pos (GridLayout *grid, UIElement *ui_element, u32 pos);
 
 // adds a new element at the end of the grid
 // this method avoids updating all the other elements positions as well
 // returns 0 on success, 1 on error
-extern u8 ui_layout_grid_add_element_at_end (GridLayout *grid, UIElement *ui_element);
+CENGINE_PUBLIC u8 ui_layout_grid_add_element_at_end (GridLayout *grid, UIElement *ui_element);
 
 // returns the ui element that is at the required position in the layout
-extern UIElement *ui_layout_grid_get_element_at (GridLayout *grid, unsigned int pos);
+CENGINE_PUBLIC UIElement *ui_layout_grid_get_element_at (GridLayout *grid, unsigned int pos);
 
 // removes an element from the grid
-extern u8 ui_layout_grid_remove_element (GridLayout *grid, UIElement *ui_element);
+CENGINE_PUBLIC u8 ui_layout_grid_remove_element (GridLayout *grid, UIElement *ui_element);
 
 // removes all ui elements from the grid layout without destroying them
-extern void ui_layout_grid_remove_ui_elements (GridLayout *grid);
+CENGINE_PUBLIC void ui_layout_grid_remove_ui_elements (GridLayout *grid);
 
 // destroys the ui elements inside the grid
-extern void ui_layout_grid_destroy_ui_elements (GridLayout *grid);
+CENGINE_PUBLIC void ui_layout_grid_destroy_ui_elements (GridLayout *grid);
 
 /*** scrolling ***/
 
 // public method to be used by user,s like to register a key to scroll up the grid
-extern void ui_layout_grid_scroll_up (GridLayout *grid, int amount);
+CENGINE_PRIVATE void ui_layout_grid_scroll_up (GridLayout *grid, int amount);
 
 // public method to be used by user,s like to register a key to scroll down the grid
-extern void ui_layout_grid_scroll_down (GridLayout *grid, int amount);
+CENGINE_PRIVATE void ui_layout_grid_scroll_down (GridLayout *grid, int amount);
 
 #endif
