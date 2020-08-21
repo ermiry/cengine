56c3b33a5e59e1e890f54f0acd5876eefc762cb7
ermiry <erick_salas@ermiry.com>
2020-08-20 23:16:14 -0500

    Updated point type header with method qualifiers

--- include/cengine/types/point2d.h
+++ include/cengine/types/point2d.h
@@ -1,26 +1,28 @@
 #ifndef _CENGINE_TYPES_POINT2D_H_
 #define _CENGINE_TYPES_POINT2D_H_
 
+#include "cengine/config.h"
+
 typedef struct Point2D {
 
     int x, y;
 
 } Point2D;
 
-extern Point2D *point_new (void);
+CENGINE_PUBLIC Point2D *point_new (void);
 
-extern Point2D *point_create (int x, int y);
+CENGINE_PUBLIC Point2D *point_create (int x, int y);
 
-extern void point_delete (void *point_ptr);
+CENGINE_PUBLIC void point_delete (void *point_ptr);
 
 // compares the two point's x and y values
 // retunrs 0 if they are exactly the same
 // uses x value to check for lower and higher values
-extern int point_x_comparator (const void *one, const void *two);
+CENGINE_PUBLIC int point_x_comparator (const void *one, const void *two);
 
 // compares the two point's x and y values
 // retunrs 0 if they are exactly the same
 // uses y value to check for lower and higher values
-extern int point_y_comparator (const void *one, const void *two);
+CENGINE_PUBLIC int point_y_comparator (const void *one, const void *two);
 
 #endif
