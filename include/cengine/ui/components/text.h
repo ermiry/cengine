05a1d79c50bfacd9efa0f95d0cc24c8827174a4a
ermiry <erick_salas@ermiry.com>
2020-08-20 23:14:57 -0500

    Updated text component header with method qualifiers

--- include/cengine/ui/components/text.h
+++ include/cengine/ui/components/text.h
@@ -36,26 +36,27 @@ typedef struct Text {
 
 } Text;
 
-extern Text *ui_text_component_new (void);
-extern void ui_text_component_delete (void *text_ptr);
+CENGINE_PUBLIC Text *ui_text_component_new (void);
+
+CENGINE_PUBLIC void ui_text_component_delete (void *text_ptr);
 
 // inits the text component with the correct values
-extern void ui_text_component_init (Text *text, Font *font, unsigned int size, RGBA_Color color, 
+CENGINE_PUBLIC void ui_text_component_init (Text *text, Font *font, unsigned int size, RGBA_Color color, 
     const char *text_str);
 
     // sets a new text for the component
-extern void ui_text_component_set_text (Text *text, const char *text_str);
+CENGINE_PUBLIC void ui_text_component_set_text (Text *text, const char *text_str);
 
 // updates the text in the component
-extern void ui_text_component_update (Text *text, const char *text_str);
+CENGINE_PUBLIC void ui_text_component_update (Text *text, const char *text_str);
 
 // sets the option to wrap text (create new lines) if to big
-extern void ui_text_component_set_wrap (Text *text, u32 wrap_lenght);
+CENGINE_PUBLIC void ui_text_component_set_wrap (Text *text, u32 wrap_lenght);
 
 // creates / updates the text texture
-extern void ui_text_component_draw (Text *text, struct _Renderer *renderer);
+CENGINE_PUBLIC void ui_text_component_draw (Text *text, struct _Renderer *renderer);
 
 // renders the text component to the screen
-extern void ui_text_component_render (Text *text, struct _Renderer *renderer);
+CENGINE_PRIVATE void ui_text_component_render (Text *text, struct _Renderer *renderer);
 
 #endif
