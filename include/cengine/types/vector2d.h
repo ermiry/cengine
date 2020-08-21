4a605ef08660830090aff6f91f20caecd6da5bca
ermiry <erick_salas@ermiry.com>
2020-08-20 23:17:05 -0500

    Updated vector type header with method qualifiers

--- include/cengine/types/vector2d.h
+++ include/cengine/types/vector2d.h
@@ -1,52 +1,64 @@
 #ifndef _CENGINE_TYPES_VECTOR2D_H_
 #define _CENGINE_TYPES_VECTOR2D_H_
 
+#include "cengine/config.h"
+
 typedef struct Vector2D { 
 
     float x, y;
 
 } Vector2D;
 
-extern const Vector2D zero_vector;
+CENGINE_PUBLIC const Vector2D zero_vector;
 
-extern Vector2D *vector_new (void);
-extern Vector2D *vector_create (float x, float y);
-extern void vector_delete (void *vector_ptr);
+CENGINE_PUBLIC Vector2D *vector_new (void);
 
-extern float vector_magnitude (Vector2D v);
-extern Vector2D vector_normalize (Vector2D vector);
+CENGINE_PUBLIC Vector2D *vector_create (float x, float y);
 
-extern Vector2D vector_negate (Vector2D v);
+CENGINE_PUBLIC void vector_delete (void *vector_ptr);
 
-extern float vector_dot (Vector2D a, Vector2D b);
+CENGINE_PUBLIC float vector_magnitude (Vector2D v);
 
-extern Vector2D vector_add (Vector2D a, Vector2D b);
-extern void vector_add_equal (Vector2D *a, Vector2D b);
+CENGINE_PUBLIC Vector2D vector_normalize (Vector2D vector);
 
-extern Vector2D vector_substract (Vector2D a, Vector2D b);
-extern void vector_substract_equal (Vector2D *a, Vector2D b);
+CENGINE_PUBLIC Vector2D vector_negate (Vector2D v);
 
-extern Vector2D vector_multiply (Vector2D v, float scalar);
-extern void vector_multiply_equal (Vector2D *v, float scalar);
+CENGINE_PUBLIC float vector_dot (Vector2D a, Vector2D b);
 
-extern Vector2D vector_divide (Vector2D v, float divisor);
-extern void vector_divide_equal (Vector2D *v, float divisor);
+CENGINE_PUBLIC Vector2D vector_add (Vector2D a, Vector2D b);
 
-extern Vector2D vector_project (Vector2D project, Vector2D onto);
-extern Vector2D unit_vector (Vector2D v);
-extern Vector2D vector_rotate (Vector2D v, float radian);
-// extern Vector2D vector_rotate_90 (Vector2D v);
+CENGINE_PUBLIC void vector_add_equal (Vector2D *a, Vector2D b);
+
+CENGINE_PUBLIC Vector2D vector_substract (Vector2D a, Vector2D b);
+
+CENGINE_PUBLIC void vector_substract_equal (Vector2D *a, Vector2D b);
+
+CENGINE_PUBLIC Vector2D vector_multiply (Vector2D v, float scalar);
+
+CENGINE_PUBLIC void vector_multiply_equal (Vector2D *v, float scalar);
+
+CENGINE_PUBLIC Vector2D vector_divide (Vector2D v, float divisor);
+
+CENGINE_PUBLIC void vector_divide_equal (Vector2D *v, float divisor);
+
+CENGINE_PUBLIC Vector2D vector_project (Vector2D project, Vector2D onto);
+
+CENGINE_PUBLIC Vector2D unit_vector (Vector2D v);
+
+CENGINE_PUBLIC Vector2D vector_rotate (Vector2D v, float radian);
+
+// CENGINE_PUBLIC Vector2D vector_rotate_90 (Vector2D v);
 
 /*** Unity like functions ***/
 
 // returns a copy of (vector) with its magnitude clamped to (maxLength)
-extern Vector2D vector_clamp_magnitude (Vector2D vector, float maxLength);
+CENGINE_PUBLIC Vector2D vector_clamp_magnitude (Vector2D vector, float maxLength);
 
 // moves a point (current) in a straight line towards a (target) point
-extern Vector2D vector_move_towards (Vector2D current, Vector2D target, float maxDistanceDelta);
+CENGINE_PUBLIC Vector2D vector_move_towards (Vector2D current, Vector2D target, float maxDistanceDelta);
 
 // gradually changes a vector towards a desired goal over time
-extern Vector2D vector_smooth_damp (Vector2D current, Vector2D target, Vector2D *currentVelocity,
+CENGINE_PUBLIC Vector2D vector_smooth_damp (Vector2D current, Vector2D target, Vector2D *currentVelocity,
     float smoothTime, float maxSpeed, float deltaTime);
 
 #endif
